6e209121c2ea2fbe685a46909bce7a86aa526c7d
Obellay <oceanebellay85340@gmail.com>
2024-11-14 15:44:13 +0100

    ajout exercice 2

---
+++ index.js
@@ -0,0 +1,12 @@
+const name = "Martin";
+const today = 2024;
+const yearsBirth = 2000;
+
+const difference = today - yearsBirth;
+// console.log(difference);
+
+const result = difference;
+
+
+console.log(`Bonjour Mr ${name} , vous avez ${result}`);
+
