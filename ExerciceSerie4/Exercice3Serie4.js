f24d392a604f9d41996c1589db9c8b4826a13485
Obellay <oceanebellay85340@gmail.com>
2024-11-19 10:16:00 +0100

    Série 4 d'exercice js

--- ExerciceSerie4/Exercice3Serie4.js
+++ ExerciceSerie4/Exercice3Serie4.js
@@ -1,10 +1,31 @@
 const tableauMots = ["je", "suis", "un" ,"tableau", "de" ,"mots"];
 
 
-function motPlusLong(tableauMots){
-    return tableauMots.reduce((plusLong, motActuel) =>{
-        return motActuel.length > plusLong.length ? motActuel : plusLong;
-    });
+// function motPlusLong(tableauMots){
+//     return tableauMots.reduce((plusLong, motActuel) =>{
+//         return motActuel.length > plusLong.length ? motActuel : plusLong;
+//     });
+// }
+
+// console.log(motPlusLong(tableauMots));
+
+
+
+function motPlusLong(words){
+
+    let result = "";
+
+    for( let word of words){
+
+        if(word.length > result.length){
+            result = word;
+        }
+       return result
+      
+        
+        
+    };
 }
 
+// motPlusLong(tableauMots);
 console.log(motPlusLong(tableauMots));
--- ExerciceSerie4/Exercice4Serie4.js
+++ ExerciceSerie4/Exercice4Serie4.js
@@ -1,10 +1,1 @@
-const mots = "bonjour";
-function inverserChaine(chaine){
-    let inverser = "";
-    for (let i = 0; i < chaine.length; i++){
-        i = chaine[i] + inverser;
-    }
-    return inverser;
-}
 
-console.log(inverserChaine("madame"));
